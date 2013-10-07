0aba958e60ed75a7f684125c1b4f95161e19b126
Christopher Pryce <cpryce@digitalriver.com>
2013-10-07 17:23:54 -0500

    expanding example

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -3,6 +3,9 @@
 var assert = require("assert");
 
 describe("Examples", function () {
+    describe(":basic example", function() {
+        assert.ok(true);
+    });
     // assert.equal
     describe(":indexOf", function() {
         it("should return -1", function() {
@@ -14,16 +17,46 @@ describe("Examples", function () {
         });
     });
 
-    // assert.notequal
+    // assert strict equal
     describe(":Shallow copy", function() {
         var ar1 = [1, 2, 3];
         var ar2 = ar1;
+        var ar3 = [1, 2, 3];
+
         it("should be equal", function() {
-            assert.equal(ar1, ar2, "these are the same.");
+            assert.equal(ar1, ar2);
+            assert.strictEqual(ar1, ar2, "these are the same.");
+            assert.deepEqual(ar1, ar2);
+        });
+
+        it("should not be equal", function() {
+            assert.notEqual(ar1, ar3);
+        });
+
+        it("should be deep equal", function() {
+            assert.deepEqual(ar1, ar3);
+            assert.deepEqual(ar1, ar2);
+        });
+
+        it("should not be strict equal", function() {
+            assert.notStrictEqual(ar1, ar3);
         });
     });
 
     describe(":async example", function () {
-        it("simple sleep");
-    })
+        it("simple sleep", function(itIsDone){
+            assert.ok(true);
+            setTimeout(function() {
+                itIsDone();
+            }, 10);
+        });
+    });
+
+    describe(":exceptions", function() {
+        it("should throw an error", function() {
+            assert.throws(function() {
+                throw new Error();
+            });
+        });
+    });
 });

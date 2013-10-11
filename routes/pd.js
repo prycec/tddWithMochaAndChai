660944e7a76e8ee582f4d3c80fe14abdc8be6c62
Christopher Pryce <cpryce@digitalriver.com>
2013-10-11 15:24:15 -0500

    added live test created in lab

--- routes/pd.js
+++ routes/pd.js
@@ -1,5 +1,42 @@
 /*global exports*/
 
+var Q = require("q"),
+        http = require("http");
+
+function getProductDetails(pid) {
+    var stringBuffer = "";
+    var json = {};
+    var rOptions = {
+        host: "store.digitalriver.com",
+        port: '80',
+        path: "/store/cpryce/en_US/DisplayDRProductInfo/productID." + pid + "/content.name+detailImage+price+buyLink+shortDescription+longDescription+product.variation/output.json/version.2",
+        method: 'GET'
+    };
+
+    var deferred = new Q.defer();
+
+    // all the async stuff goes here
+    http.get(rOptions, function (xhr) {
+        xhr.setEncoding('utf8');
+        xhr.on('data', function (chunk) {
+            stringBuffer += chunk;
+        });
+
+        xhr.on('end', function () {
+            try {
+                json = JSON.parse(stringBuffer);
+            } catch (e) {
+                deferred.reject("can't parse response as JSON");
+            }
+
+            deferred.resolve(json);
+        });
+    });
+    return deferred.promise;
+}
+
 exports.index = function(req, res) {
     res.send("called with product id: " + req.params.pid);
 };
+
+exports.getProductDetails = getProductDetails;
---
+++ test/pd-test.js
@@ -0,0 +1,20 @@
+/*global describe it*/
+var pd = require("../routes/pd");
+var assert = require("assert");
+
+describe("getProductDetails", function() {
+    describe(":should return a promise", function() {
+        var promise = pd.getProductDetails('281246600');
+         it("is a promise", function() {
+            assert.ok(promise.then);
+         });
+
+        it("should be vaild JSON", function(itIsDone) {
+            promise.then(function(data) {
+                // create
+                assert.ok(data.productInfo);
+                itIsDone();
+            });
+        });
+    })
+});

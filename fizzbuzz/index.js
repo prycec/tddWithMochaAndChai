2aeefdff9e4843e41f4d4913c6e405fca855643f
stuller <stuller@digitalriver.com>
2014-03-07 14:14:14 -0600

    removing all tests except examples

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -5,17 +5,6 @@
 module.exports = {
     fizzbuzz: function () {
         var fizzBuzzArray = [];
-        for (var i = 1; i < 101; i++) {
-            if (i % 3 === 0 && i % 5 === 0) {
-                fizzBuzzArray.push("fizzbuzz");
-            } else if (i % 3 === 0) {
-                fizzBuzzArray.push("fizz");
-            } else if (i % 5 === 0) {
-                fizzBuzzArray.push("buzz");
-            } else {
-                fizzBuzzArray.push(i);
-            }
-        }
         return fizzBuzzArray;
     }
 };
